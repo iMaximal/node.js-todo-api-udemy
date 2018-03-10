53f4d783bfaa8a1e8f9e1598043c1bccdb41cbbe
Maximal <maxim@infofree.ru>
2018-03-10 16:27:16 +0500

    add User schema

--- server/server.js
+++ server/server.js
@@ -1,42 +1,57 @@
 const mongoose = require('mongoose')
-
+const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost:27017/TodoApp')
 
-const Todo = mongoose.model('Todo', {
+
+const Todo = mongoose.model('Todo', new Schema({
   text: {
     type: String,
+    required: true,
+    minlength: 1,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
   completedAt: {
     type: Number,
+    default: null,
   },
+}))
+
+
+// const otherTodo = new Todo({})
+//
+// otherTodo.save()
+//   .then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2))
+//   })
+//   .catch((error) => {
+//     console.log('Unable to save', error)
+//   })
+
+
+// User
+// email - require it - trim it - set type - set min length of 1
+const User = mongoose.model('User', new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+}))
+
+const user = new User({
+  email: 'max@example.com',
 })
 
-const newTodo = new Todo({
-  text: 'Cook dinner',
-})
-
-newTodo.save()
+user.save()
   .then((doc) => {
-    console.log('Saved todo', doc)
+    console.log('User saved', doc)
   })
   .catch((error) => {
-    console.log('Unable to save todo', error)
-  })
-
-const otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123,
-})
-
-otherTodo.save()
-  .then((doc) => {
-    console.log(JSON.stringify(doc, undefined, 2))
-  })
-  .catch((error) => {
-    console.log('Unable to save', error)
+    console.log('Unable to save user', error)
   })
