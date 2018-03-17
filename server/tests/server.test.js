f5feea853fe2e6bd660a3672315ab5e25ccd33b9
Maksim Golitsyn <maxim@infofree.ru>
2018-03-17 13:41:49 +0500

    refactoring tests structure

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,26 +3,10 @@ const request = require('supertest')
 const { ObjectID } = require('mongodb')
 const { app } = require('../server')
 const { Todo } = require('../models/todo')
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed')
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo',
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333,
-}]
-
-beforeEach((done) => {
-  Todo.remove({})
-    .then(() => Todo.insertMany(todos, (error, docs) => {
-      if (error) {
-        return done(error)
-      }
-    }))
-    .then(() => done())
-})
+beforeEach(populateUsers)
+beforeEach(populateTodos)
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
