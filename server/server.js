791678bb6adbb80f0586eac195969fd3828b2aba
Maksim Golitsyn <maxim@infofree.ru>
2018-03-17 12:14:39 +0500

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ObjectID } = require('mongodb')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+const { authenticate } = require('./middleware/authenticate')
 
 const PORT = process.env.PORT || 3000
 
@@ -116,9 +117,7 @@ app.post('/users', (req, res) => {
   const user = new User(body)
 
   user.save()
-    .then(() => {
-      return user.generateAuthToken()
-    })
+    .then(() => user.generateAuthToken())
     .then((token) => {
       res.header('x-auth', token).send(user)
     })
@@ -126,6 +125,12 @@ app.post('/users', (req, res) => {
       res.status(400).send(e)
     })
 })
+
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
+})
+
 
 app.listen(PORT, () => {
   console.log('Started on port 3000')
