090a914f01b78a4e475456b899e5eec87977daa8
Maksim Golitsyn <maxim@infofree.ru>
2018-03-18 16:14:55 +0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
     minlength: 1,
     unique: true,
     validate: {
-      validator: (value) => validator.isEmail,
+      validator: validator.isEmail,
       message: '{VALUE} is not a valid email!',
     },
   },
@@ -67,6 +67,28 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth',
   })
+}
+
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this
+
+  return User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject()
+      }
+
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user)
+          } else {
+            reject()
+          }
+        })
+      })
+    })
 }
 
 UserSchema.pre('save', function (next) {
--- server/server.js
+++ server/server.js
@@ -132,6 +132,23 @@ app.get('/users/me', authenticate, (req, res) => {
 })
 
 
+// POST /users/login { email, password }
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+
+  User.findByCredentials(body.email, body.password)
+    .then((user) => {
+      return user.generateAuthToken()
+        .then((token) => {
+          res.header('x-auth', token).send(user)
+        })
+    })
+    .catch((e) => {
+      res.status(400).send()
+    })
+})
+
+
 app.listen(PORT, () => {
   console.log('Started on port 3000')
 })
