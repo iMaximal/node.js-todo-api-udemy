52c5428524dc606c697a969a3cc84507baf24c07
Maksim Golitsyn <maxim@infofree.ru>
2018-03-24 17:33:49 +0500

    Add async/await. Finish dev

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
-const { mongoose } = require('./db/mongoose')
+const { mongoose } = require('./db/mongoose') // no delete, it's for tests!
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
 const { authenticate } = require('./middleware/authenticate')
@@ -66,27 +66,28 @@ app.get('/todos/:id', authenticate, (req, res) => {
 })
 
 
-app.delete('/todos/:id', authenticate, (req, res) => {
+app.delete('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
 
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id,
-  })
-    .then((todo) => {
-      if (!todo) {
-        return res.status(404).send()
-      }
+  try {
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id,
+    })
 
-      res.send({ todo })
-    })
-    .catch((e) => {
-      res.status(400).send()
-    })
+
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({ todo })
+  } catch (e) {
+    res.status(400).send()
+  }
 })
 
 
@@ -120,18 +121,16 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 
 // POST /users
-app.post('/users', (req, res) => {
-  const body = _.pick(req.body, ['email', 'password'])
-  const user = new User(body)
-
-  user.save()
-    .then(() => user.generateAuthToken())
-    .then((token) => {
-      res.header('x-auth', token).send(user)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+app.post('/users', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password'])
+    const user = new User(body)
+    await user.save()
+    const token = await user.generateAuthToken()
+    res.header('x-auth', token).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 
@@ -141,26 +140,25 @@ app.get('/users/me', authenticate, (req, res) => {
 
 
 // POST /users/login { email, password }
-app.post('/users/login', (req, res) => {
-  const body = _.pick(req.body, ['email', 'password'])
-
-  User.findByCredentials(body.email, body.password)
-    .then((user) => {
-      return user.generateAuthToken()
-        .then((token) => {
-          res.header('x-auth', token).send(user)
-        })
-    })
-    .catch((e) => {
-      res.status(400).send()
-    })
+app.post('/users/login', async (req, res) => {
+  try {
+    const body = _.pick(req.body, ['email', 'password'])
+    const user = await User.findByCredentials(body.email, body.password)
+    const token = await user.generateAuthToken()
+    res.header('x-auth', token).send(user)
+  } catch (e) {
+    res.status(400).send()
+  }
 })
 
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token)
-    .then(() => res.status(200).send())
-    .catch(() => res.status(400).send())
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try {
+    await req.user.removeToken(req.token)
+    res.status(200).send()
+  } catch (e) {
+    res.status(400).send()
+  }
 })
 
 app.listen(PORT, () => {
